95e8f5f5456cf4b56a6aa470f4e36d3cb9ee21c5
Marcus Koh <kohchihao@gmail.com>
2019-02-13 00:44:40 +0800

    hooks.js: Add error catching

--- src/hooks.js
+++ src/hooks.js
@@ -1,26 +1,31 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 import rp from 'request-promise';
 
 const useFetch = url => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
 
   const fetchData = async () => {
-
-    let options = {
-      uri: url,
-      json: true // Automatically parses the JSON string in the response
-    };
-    const response = await rp(options);
-    setData(response);
-    setLoading(false);
+    try {
+      let options = {
+        uri: url,
+        json: true // Automatically parses the JSON string in the response
+      };
+      const response = await rp(options);
+      setData(response);
+      setLoading(false);
+    } catch (err) {
+      setError(err.message);
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  return [data, loading];
+  return [data, loading, error];
 };
 
 export { useFetch };
