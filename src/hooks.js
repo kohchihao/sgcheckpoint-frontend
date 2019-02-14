5f972d49212289705ec7500c011f9cd860f6a6ba
Marcus Koh <kohchihao@gmail.com>
2019-02-14 15:29:57 +0800

    hooks.js: Remove useCounter()

--- src/hooks.js
+++ src/hooks.js
@@ -28,4 +28,10 @@ const useFetch = url => {
   return [data, loading, error];
 };
 
-export { useFetch };
+function useCounter({initialCount = 0, step = 1} = {}) {
+  const [count, setCount] = useState(initialCount)
+  const increment = () => setCount(c => c + step)
+  const decrement = () => setCount(c => c - step)
+  return {count, increment, decrement}
+}
+export { useFetch, useCounter};
